80fc5365d8fed1f94aeb546c659eab6e80ead3d6
danbriechle <danbriechle@hotmail.com>
2025-11-24 15:24:48 -0700

    Backend - Fix test env hosts

--- backend/config/environments/test.rb
+++ backend/config/environments/test.rb
@@ -50,4 +50,8 @@ Rails.application.configure do
 
   # Raise error when a before_action's only/except options reference missing actions.
   config.action_controller.raise_on_missing_callback_actions = true
+  
+  # Allow the default RSpec host in test
+  config.hosts.clear
+  config.hosts << "www.example.com"
 end
